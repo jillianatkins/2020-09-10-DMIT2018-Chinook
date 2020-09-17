498b670f06e6e5cf7de63d4dd339cbea6f93ed31
robbinl <robbinl@telusplanet.net>
2020-09-17 15:33:02 -0600

    Front end of CGVofTracks

--- WebApp/SamplePages/P04_CGVofTracks.aspx.cs
+++ WebApp/SamplePages/P04_CGVofTracks.aspx.cs
@@ -13,5 +13,11 @@ namespace WebApp.SamplePages
         {
 
         }
+        protected void GridView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            GridViewRow agvrow = GridView1.Rows[GridView1.SelectedIndex];
+            string colValue = agvrow.Cells[1].Text.Trim();
+            MessageLabel.Text = colValue;
+        }
     }
 }
