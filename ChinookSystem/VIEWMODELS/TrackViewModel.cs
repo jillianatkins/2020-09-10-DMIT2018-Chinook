a503d695ac22ff94c1e4a44d26c2b01656258043
robbinl <robbinl@telusplanet.net>
2020-09-17 12:34:31 -0600

    Build Track Entity, ViewModel, and Context.

---
+++ ChinookSystem/VIEWMODELS/TrackViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChinookSystem.VIEWMODELS
+{
+    public class TrackViewModel
+    {
+        public string Name { get; set; }
+        public string Composer { get; set; }
+        public int Milliseconds { get; set; }
+        public int? Bytes { get; set; }
+        public decimal UnitPrice { get; set; }
+    }
+}
