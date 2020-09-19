fa191fd9c7a72dd0caf4a52aeb58be1620c2c667
robbinl <robbinl@telusplanet.net>
2020-09-19 13:26:43 -0600

    fix

--- WebApp/SamplePages/P02_DDLofAlbums.aspx.designer.cs
+++ WebApp/SamplePages/P02_DDLofAlbums.aspx.designer.cs
@@ -42,6 +42,15 @@ namespace WebApp.SamplePages
         protected global::System.Web.UI.WebControls.Button Button1;
 
         /// <summary>
+        /// MessageLabel control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label MessageLabel;
+
+        /// <summary>
         /// Album_ODS control.
         /// </summary>
         /// <remarks>
