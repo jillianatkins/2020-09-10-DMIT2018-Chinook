a8a30124d2732038f3edd0e997724df55f32bfdf
robbinl <robbinl@telusplanet.net>
2020-09-19 13:33:02 -0600

    fix

--- WebApp/SamplePages/P00_TestPage.aspx.cs
+++ WebApp/SamplePages/P00_TestPage.aspx.cs
@@ -7,7 +7,7 @@ using System.Web.UI.WebControls;
 
 namespace WebApp.SamplePages
 {
-    public partial class TestPage1 : System.Web.UI.Page
+    public partial class P00_TestPage : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
