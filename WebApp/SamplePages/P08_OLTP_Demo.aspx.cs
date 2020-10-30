c38feca661ee6ad380efe6324dc0e60569e53daf
robbinl <robbinl@telusplanet.net>
2020-10-30 17:54:11 -0600

    a

--- WebApp/SamplePages/P08_OLTP_Demo.aspx.cs
+++ WebApp/SamplePages/P08_OLTP_Demo.aspx.cs
@@ -13,5 +13,53 @@ namespace WebApp.SamplePages
         {
 
         }
+
+        protected void TracksSelectionList_ItemCommand(object sender,
+            ListViewCommandEventArgs e)
+        {
+            //code to go here
+
+        }
+
+        #region Error Handling
+        protected void CheckForException(object sender, ObjectDataSourceStatusEventArgs e)
+        {
+            MessageUserControl.HandleDataBoundException(e);
+        }
+
+        protected void InsertCheckForException(object sender, ObjectDataSourceStatusEventArgs e)
+        {
+            if (e.Exception == null)
+            {
+                MessageUserControl.ShowInfo("Success", "Album has been added.");
+            }
+            else
+            {
+                MessageUserControl.HandleDataBoundException(e);
+            }
+        }
+        protected void UpdateCheckForException(object sender, ObjectDataSourceStatusEventArgs e)
+        {
+            if (e.Exception == null)
+            {
+                MessageUserControl.ShowInfo("Success", "Album has been updated.");
+            }
+            else
+            {
+                MessageUserControl.HandleDataBoundException(e);
+            }
+        }
+        protected void DeleteCheckForException(object sender, ObjectDataSourceStatusEventArgs e)
+        {
+            if (e.Exception == null)
+            {
+                MessageUserControl.ShowInfo("Success", "Album has been removed.");
+            }
+            else
+            {
+                MessageUserControl.HandleDataBoundException(e);
+            }
+        }
+        #endregion
     }
 }
